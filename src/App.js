f85d611d8acc453954c796c342bdb9112fa399a9
Castellegeek <108758585+Castellegeek@users.noreply.github.com>
2022-10-10 23:35:10 +0200

    ReactJS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./Style.css";
+import "bootstrap/dist/css/bootstrap.css";
+import LoginForm from "./Components/LoginForm";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
